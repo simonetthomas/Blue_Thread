217f2ec88eba801bbbf5d75a7de774c52d9aef13
Thomas Simonet <thomas-tm@hotmail.fr>
2023-10-21 21:08:34 +0200

    v0.1.6 - Fixed #39 : Thread gets posted with a different account

--- server.py
+++ server.py
@@ -13,9 +13,6 @@ app.config["SESSION_TYPE"] = "filesystem"
 app.config["SESSION_USE_SIGNER"] = "True"
 Session(app)
 
-client = Client()
-profile = None  # Permet de garder les infos du profil bluesky
-
 @app.route('/')
 def index():
     print('/')
@@ -28,8 +25,7 @@ def index():
 
 @app.route("/login", methods=['GET', 'POST'])
 def fctn_login():
-    global client
-    global profile
+    client = Client()
 
     print ("/login")
     if request.method == 'POST':
@@ -37,11 +33,9 @@ def fctn_login():
         password = request.form.get('password')
 
         if (connexion(client, login, password) == 0):
-            session["name"] = request.form.get('login')
             return redirect('/thread')
         else:
             error="Erreur de connexion, veuillez vérifier l'identifiant et le mot de passe."
-            session["name"] = None
             return render_template("index.html", error=error)
 
     elif request.method == 'GET':
@@ -57,11 +51,10 @@ def fctn_login():
 
 @app.route("/logout", methods=['GET'])
 def logout():
-    global profile
-
     print("- Déconnexion")
-    profile = None;
     session["name"] = None
+    session["password"] = None
+    session["profile"] = None
     return redirect('/')
 
 
@@ -70,8 +63,6 @@ def thread():
     print ('/thread')
     thread=[]
     disabled="disabled='true'"
-    global client
-    global profile
 
     if not session.get("name"):
         print("Utilisateur non connecté, redirection vers la page de connexion")
@@ -89,9 +80,9 @@ def thread():
             #alts=request.form.getlist("alt")
             #print("alts : ", alts)
 
-            if (profile is not None):   # Si le client a bien une connexion valide à Bluesky
-                if (envoi_thread(thread, images, request.form, client) == 0):
-                    print("Thread envoyé sur le compte bluesky de " + profile.handle + "!\n")
+            if (session.get("profile") is not None):   # Si le client a bien une connexion valide à Bluesky
+                if (envoi_thread(thread, images, request.form) == 0):
+                    print("Thread envoyé sur le compte bluesky de " + session.get("name") + " !")
                     return render_template('thread_sent.html')
                 else :
                     print("Erreur lors de l'envoi...")
@@ -113,78 +104,92 @@ def page_not_found(error):
 # Prend en entrée le client, un login et un password
 # Retourne 0 si connexion ok, 1 sinon
 def connexion(client, login, password):
-    global profile
     try:
         print("- Connexion...")
         profile = client.login(login, password)
+        session["profile"] = profile    # Permet de garder les infos du profil bluesky
+        session["name"] = login
+        session["password"] = password
     except Exception as error:
         print("Erreur de connexion. Veuillez vérifier l'identifiant et le mot de passe.", type(error).__name__, error)
-        profile = None;
+        session["profile"] = None
+        session["name"] = None
+        session["password"] = None
         return 1;
     else:
-        print("- Connecté en tant que "+profile.handle)
+        print("- Connecté en tant que "+session.get("profile").handle)
         return 0;
 
 
 # Poste le thread sur Bluesky
 # Prend en entree un array de strings, des images, les valeurs du formulaire et un client
 # retourne 0 si ok
-def envoi_thread (thread, images, form, client):
+def envoi_thread (thread, images, form):
     premier=True
     str_nb_posts = str(len(thread))
     langue = form.get("lang")
     
-    print("Envoi_thread : ", thread)
+    client = Client()
+    login=session["name"]
+    password=session["password"]
+    
+    print("- Envoi_thread : ", thread)
+    
+    if (connexion(client, login, password) == 0):
 
-    for index, post in enumerate(thread):
-        numerotation = str(index+1)+"/"+str_nb_posts
-        embed=''
-        
-        try:    # Essai de recupation d'un alt pour ce post
-            alt = form.get("alt"+str(index+1))
-        except Exception:
-            alt = ""  # no alt for this post
-            #print("pas de alt récupéré")
-        
-        if (premier):
-            # Envoi du premier post, qui ne fait référence à aucun post
-            # print("image", images[index])
-            if (images[index].filename==''):    # Si pas d'image
-                #print("pas d'image")
-                root_post_ref = models.create_strong_ref(client.send_post(text=post, langs=[langue]))
+        for index, post in enumerate(thread):
+            numerotation = str(index+1)+"/"+str_nb_posts
+            embed=''
+            
+            try:    # Essai de recupation d'un alt pour ce post
+                alt = form.get("alt"+str(index+1))
+            except Exception:
+                alt = ""  # no alt for this post
+                #print("pas de alt récupéré")
+            
+            if (premier):
+                # Envoi du premier post, qui ne fait référence à aucun post
+                # print("image", images[index])
+                if (images[index].filename==''):    # Si pas d'image
+                    #print("pas d'image")
+                    root_post_ref = models.create_strong_ref(client.send_post(text=post, langs=[langue]))
 
-            else:   # S'il y a une image
-                print("Premier post avec une image")
-                img_data=images[index].read()
-                print("Juste avant ''upload_blob''")
-                upload = client.com.atproto.repo.upload_blob(img_data)
-                print("Juste après ''upload_blob''")
-                pics = [models.AppBskyEmbedImages.Image(alt=alt, image=upload.blob)]
-                embed = models.AppBskyEmbedImages.Main(images=pics)
-                
-                root_post_ref = models.create_strong_ref(client.send_post(
-                    text=post, langs=[langue], embed=embed
-                ))
-                                        
-            parent_post_ref = root_post_ref     # Le premier post devient la ref du post parent
-            premier=False
-        else:
-            # Envoi d'un post suivant, en réponse au précedent
-            if (images[index].filename==''):    # Si pas d'image
-                parent_post_ref = models.create_strong_ref(client.send_post(
-                    text=post, reply_to=models.AppBskyFeedPost.ReplyRef(parent=parent_post_ref, root=root_post_ref), langs=[langue]
-                ))
-            else:   # S'il y a une image
-                img_data=images[index].read()
-                upload = client.com.atproto.repo.upload_blob(img_data)
-                pics = [models.AppBskyEmbedImages.Image(alt=alt, image=upload.blob)]
-                embed = models.AppBskyEmbedImages.Main(images=pics)
-                
-                parent_post_ref = models.create_strong_ref(client.send_post(
-                    text=post, reply_to=models.AppBskyFeedPost.ReplyRef(parent=parent_post_ref, root=root_post_ref), langs=[langue], embed=embed
-                ))
-                
-        print("- Post "+numerotation+" envoyé")
+                else:   # S'il y a une image
+                    print("Premier post avec une image")
+                    img_data=images[index].read()
+                    print("Juste avant ''upload_blob''")
+                    upload = client.com.atproto.repo.upload_blob(img_data)
+                    print("Juste après ''upload_blob''")
+                    pics = [models.AppBskyEmbedImages.Image(alt=alt, image=upload.blob)]
+                    embed = models.AppBskyEmbedImages.Main(images=pics)
+                    
+                    root_post_ref = models.create_strong_ref(client.send_post(
+                        text=post, langs=[langue], embed=embed
+                    ))
+                                            
+                parent_post_ref = root_post_ref     # Le premier post devient la ref du post parent
+                premier=False
+            else:
+                # Envoi d'un post suivant, en réponse au précedent
+                if (images[index].filename==''):    # Si pas d'image
+                    parent_post_ref = models.create_strong_ref(client.send_post(
+                        text=post, reply_to=models.AppBskyFeedPost.ReplyRef(parent=parent_post_ref, root=root_post_ref), langs=[langue]
+                    ))
+                else:   # S'il y a une image
+                    img_data=images[index].read()
+                    upload = client.com.atproto.repo.upload_blob(img_data)
+                    pics = [models.AppBskyEmbedImages.Image(alt=alt, image=upload.blob)]
+                    embed = models.AppBskyEmbedImages.Main(images=pics)
+                    
+                    parent_post_ref = models.create_strong_ref(client.send_post(
+                        text=post, reply_to=models.AppBskyFeedPost.ReplyRef(parent=parent_post_ref, root=root_post_ref), langs=[langue], embed=embed
+                    ))
+                    
+            print("- Post "+numerotation+" envoyé")
+            
+    else:
+        print("Erreur de connexion du client lors de l'envoi...")
+        flash ("Erreur de connexion du client lors de l'envoi...")
 
     return 0
 
